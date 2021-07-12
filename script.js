803ba866319b34e5e096e6abd5a581bc96c51454
ArisC1 <67166970+Aris-C@users.noreply.github.com>
2021-07-12 18:31:20 +0000

    started it

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // Author:
 
 // Global UI Variables
@@ -11,15 +12,39 @@ let textP2;
 let soundClassifier;
 
 function setup() {
-
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  textDiv = createDiv();
+  textP = createP("Model loading plaese wait");
+  textP.parent(textDiv);
+  textP2 = createP();
+  textP.parent(textDiv);
+  //configure options
+  let options = {
+    probabilityThreshold: 0.95
+  };
+  soundClassifier = ml5.soundClassifier("SpeachCommands18w", options, modelReady);
 }
 
 function draw() {
+  let label = textP.html();
+  if(label.includes("up")) {
+    background(190, 0, 40);
 
+  } else if (label.includes("down")){
+      background(0, 255, 0);
+  } else if (label.includes("left")){
+      background(0, 0, 255);
+  
+  } else if (label.includes("right")){
+      background(180, 30, 100);
+  }
 }
 
 function modelReady() {
-
+  textP.html("Model loaded, say anything below!");
+  textP2.html("<b>Commands</b>: Numbers 0 through 9, up, down, left, right, go, stop, yes, no");
+  soundClassifier.classify(gotResults);
 }
 
 function gotResults(error, results) {
@@ -27,8 +52,9 @@ function gotResults(error, results) {
     console.error(error);
   } else {
     // Complete the code below
-    let label;
-    let confidence;
+    let label = results[0].label;
+    let confidence  = round(results[0].confidence, 2) * 100;
+    textp.html(label + ": " + confidence + "% ")
 
   }
 }
